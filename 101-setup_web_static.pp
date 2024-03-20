6cffdcf345bcbd196c9539aaa36060b54e0a3d5f
kelvindaudi <kelvindaudi98@gmail.com>
2024-03-20 05:52:20 -0700

    my sql

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,18 @@
+# Configures a web server for deployment of web_static.
+
+# Nginx configuration file
+$nginx_conf = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
